e81edb6bea4a7d7bb0024bf73f83fff711db707d
Anna <scheul93@gmail.com>
2017-11-11 17:05:32 -0600

    base zip code search elements

---
+++ src/app/search/zipcode-search/zipcode-search.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { SearchService } from '../shared/search.service';
+
+@Component({
+  selector: 'zipcode-search',
+  templateUrl: './zipcode-search.component.html',
+  styleUrls: ['./zipcode-search.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ZipcodeSearchComponent implements OnInit {
+
+  constructor(private searchSvc: SearchService) { }
+
+  ngOnInit() {}
+
+  searchDoctors(){
+    this.searchSvc.searchDoctors();
+  }
+
+}
