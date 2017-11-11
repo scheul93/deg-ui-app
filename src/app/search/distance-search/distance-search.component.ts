2a57552a75579a3be8a24ab830d8bbd48a60f255
Anna <scheul93@gmail.com>
2017-11-11 17:06:41 -0600

    base distance search elements

---
+++ src/app/search/distance-search/distance-search.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'distance-search',
+  templateUrl: './distance-search.component.html',
+  styleUrls: ['./distance-search.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class DistanceSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
