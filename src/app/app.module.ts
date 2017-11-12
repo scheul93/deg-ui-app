115cf9719de0d8f257810a981ce55bfccc810a41
Anna <scheul93@gmail.com>
2017-11-11 21:56:34 -0600

    main page updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
+//Components
+import { DistanceSearchComponent } from './search/distance-search/distance-search.component';
+import { GenderSearchComponent } from './search/gender-search/gender-search.component';
+import { ZipcodeSearchComponent } from './search/zipcode-search/zipcode-search.component';
+import { RadioButtonsComponent } from './shared/radio-buttons/radio-buttons.component';
+import { DoctorListComponent } from './doctors/doctor-list/doctor-list.component';
+import { DoctorComponent } from './doctors/doctor/doctor.component';
+
+//Services
+import { SearchService } from './search/shared/search.service';
+import { HttpModule } from '@angular/http';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DistanceSearchComponent,
+    GenderSearchComponent,
+    ZipcodeSearchComponent,
+    RadioButtonsComponent,
+    DoctorListComponent,
+    DoctorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    SearchService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
